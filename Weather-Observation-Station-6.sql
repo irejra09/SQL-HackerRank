a4d45bfe146f6f111cc70ce5294f225c3487bff6
Ishaan Rejra <ishaanrejra@gmail.com>
2024-02-16 12:03:34 +0530

    Create Weather-Observation-Station-6.sql

---
+++ Weather-Observation-Station-6.sql
@@ -0,0 +1,3 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTR(CITY,1,1) IN ('a','e','i','o','u');
