3f70d119d73f0f5c99b2ada408a21495919ac30e
Ishaan Rejra <ishaanrejra@gmail.com>
2024-02-18 21:49:54 +0530

    Create Weather-Observation-Station-12.sql

---
+++ Weather-Observation-Station-12.sql
@@ -0,0 +1,3 @@
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE SUBSTR(CITY,1,1) NOT IN ('a','e','i','o','u') AND SUBSTR(CITY,LENGTH(CITY),LENGTH(CITY)) NOT IN ('a','e','i','o','u');
